7e8df8bbe53cb2e8755e158871858e80587a3111
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-12 15:43:18 +0100

    Popular component started

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide'
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import '@splidejs/react-splide/css'
 
 const Popular = () => {
   const [popular, setPopular] = useState([])
@@ -15,16 +18,39 @@ const Popular = () => {
   }, [])
 
   return (
-    <>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        )
-      })}
-    </>
+    <Wrapper>
+      <h3>Popular Picks</h3>
+      <Splide
+        options={{
+          perPage: 4,
+        }}
+      >
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide>
+              <Card key={recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </Card>
+            </SplideSlide>
+          )
+        })}
+      </Splide>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`
 
 export default Popular
