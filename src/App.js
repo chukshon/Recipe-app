e1f13bfd45f3220215558dc497e87754662ba5c8
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-13 15:46:25 +0100

    app almost done animation left

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter } from 'react-router-dom'
+import { GiKnifeFork } from 'react-icons/gi'
+import { BrowserRouter, Link } from 'react-router-dom'
+import styled from 'styled-components'
 import Category from './components/Category'
 import Search from './components/Search'
 import Home from './page/Home'
@@ -6,12 +8,34 @@ import Pages from './page/Pages'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Search />
-      <Category />
-      <Pages />
-    </BrowserRouter>
+    <div className='app'>
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={'/'}>Food Delight</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
   )
 }
 
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`
 export default App
--- src/page/Cuisine.jsx
+++ src/page/Cuisine.jsx
@@ -18,7 +18,12 @@ const Cuisine = () => {
     getCuisines(params.type)
   }, [params.type])
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ opacity: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
@@ -33,7 +38,7 @@ const Cuisine = () => {
   )
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
