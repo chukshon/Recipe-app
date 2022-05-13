ded799db291579ccb200e0f14520ab785f3c922a
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-05-13 13:36:14 +0100

    search component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import Category from './components/Category'
+import Search from './components/Search'
 import Home from './page/Home'
 import Pages from './page/Pages'
 
 function App() {
   return (
     <BrowserRouter>
+      <Search />
       <Category />
       <Pages />
     </BrowserRouter>
---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import styled from 'styled-components'
+
+const Search = () => {
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+  }
+  return (
+    <FormStyle onSubmit={handleSubmit}>
+      <div>
+        <input type='text' value={value} onChange={handleChange} />
+        <FaSearch></FaSearch>
+      </div>
+    </FormStyle>
+  )
+}
+
+const FormStyle = styled.form`
+  margin: 0rem;
+  div {
+    width: 100%;
+    position: relative;
+  }
+  input {
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+    width: 100%;
+    color: white;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`
+export default Search
